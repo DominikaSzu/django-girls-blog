110ebec0a13771557e0797b325ddd667517eb683
DominikaSzu <dominika.szukala@gmail.com>
2019-09-28 15:27:55 +0200

    fix" "

--- blog/models.py
+++ blog/models.py
@@ -12,9 +12,9 @@ class Post(models.Model):
 	published_date = models.DateTimeField(
 		blank=True, null=True)
 
-def publish(self):
-	self.published_date = timezone.now()
-	self.save()
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.title
