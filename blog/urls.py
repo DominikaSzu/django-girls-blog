9baf86c45e7d6f9980f6f7f2fa41df66b98ac7d8
DominikaSzu <dominika.szukala@gmail.com>
2019-09-28 17:26:01 +0200

    posty

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-]
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
